cc675d1c68625d81abe402ad26d6e5927512b5c5
alikadev <dev@alikadev.com>
2024-04-30 19:55:00 +0200

    Add --help and --version arguments

--- Makefile
+++ Makefile
@@ -4,9 +4,16 @@ CC   := cc
 SRC_DIR     := src
 INCLUDE_DIR := include
 PLAYM       := playm
+PLAYM_MAJOR := 0
+PLAYM_MINOR := 1
+PLAYM_PATCH := 6
 SRC         := $(shell find $(SRC_DIR) -name '*.c')
 MIXERFLAGS  := $(shell pkg-config --cflags --libs SDL2_mixer)
-CFLAGS      := -Werror -Wextra -Wall -std=c99 -I$(INCLUDE_DIR) $(MIXERFLAGS)
+CFLAGS      := -Werror -Wextra -Wall -std=c99 
+CFLAGS      += -I$(INCLUDE_DIR) $(MIXERFLAGS) 
+CFLAGS      += -DVERSION_MAJOR=$(PLAYM_MAJOR) 
+CFLAGS      += -DVERSION_MINOR=$(PLAYM_MINOR)
+CFLAGS      += -DVERSION_PATCH=$(PLAYM_PATCH)
 
 .PHONY: $(PLAYM) clean
 
--- src/main.c
+++ src/main.c
@@ -9,10 +9,43 @@
 #include <pm/sys.h>
 #include <pm/audio.h>
 
+bool parse_args(int argc, const char *argv[])
+{
+    assert(argc > 0 && "Must have at least 1 argument");
+    assert(argv && "argv must not be NULL");
+    assert(*argv && "Must have at least 1 argument in ARGV");
+
+    const char *program = *argv;
+    for (int i = 1; i < argc; ++i)
+    {
+        if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0)
+        {
+            printf("Usage: %s (-v / --version)\n", program);
+            printf("Flags\n");
+            printf("  -v / --version: Print current version\n");
+            exit(0);
+        }
+        else if (strcmp(argv[i], "-v") == 0 
+                || strcmp(argv[i], "--version") == 0)
+        {
+            printf("PlayM Version %d.%d.%d\n",
+                    VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH);
+            exit(0);
+        }
+        else
+        {
+            fprintf(stderr, "Unknown argument `%s`\n", argv[i]);
+            return false;
+        }
+    }
+
+    return true;
+}
+
 int main(int argc, const char *argv[])
 {
-    (void) argc;
-    (void) argv;
+    if (!parse_args(argc, argv))
+        return 1;
 
     AppState state = {
         .running = true,
